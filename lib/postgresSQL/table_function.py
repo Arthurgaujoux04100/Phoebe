92f3e305a258d9529e7fefaca6a0c6c979d6dcbb
Arthurgaujoux04100 <arthur.gaujoux.delahaye@gmail.com>
2024-12-13 16:13:33 -0800

    change the type return of the get_bar_name : it was a list, now it's a dictionnary with all the information related

--- lib/postgresSQL/table_function.py
+++ lib/postgresSQL/table_function.py
@@ -4,20 +4,27 @@ from sqlalchemy import create_engine
 from lib.postgresSQL.connexion import connexion_postresql
 
 
-def get_bar_name(city_user:list)->list:
+def get_bar_name(city_user:list)->dict:
     """
-    get the bar name from the postgresql database 
+    get the bar name from the postgresql database
+    to_dict(orient='records'): This method converts each row of the DataFrame into a dictionary, where the keys are the column names and the values are the corresponding values of the row.
+     The parameter orient='records' specifies that each row should be a separate dictionary.
+    Returns a list of dictionaries: The function returns a list where each element is a dictionary representing a row from the city_bar table.
     city_user = city from the user
     return :list
     """
+    if len(city_user)>1:
+        city = tuple(city_user)
+    else:
+        city =  "('" + ', '.join(city_user) + "')"
     engine = connexion_postresql()
     query = f"""
         select 
             city_name,
             bar_name
         from city_bar
-        where city_name in {tuple(city_user)}"""
-    return  pd.read_sql(query, engine).values.tolist()
+        where city_name in {city}"""
+    return  pd.read_sql(query, engine).to_dict(orient='records')
 
 
 def get_all_french_cities()->list:
