38107d7d9ccc484a4b090bf2b95f0a48d783dda9
Arthurgaujoux04100 <arthur.gaujoux.delahaye@gmail.com>
2024-12-16 11:25:01 -0800

    first RAG working

--- lib/model_IA/gpt/prompt_function.py
+++ lib/model_IA/gpt/prompt_function.py
@@ -7,7 +7,7 @@ def select_prompt_path(flag_prompt: int)->str:
     return
         prompt str : str
     """
-    base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+    base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
     prompt_dir = os.path.join(base_path, 'prompts')  
     if flag_prompt == 0:
         return os.path.join(base_path, 'prompts', 'Phoebe_analyse_detailed.txt')
@@ -31,4 +31,4 @@ def replace_rag_info_in_prompt(rag_info:dict, prompt:str)->str:
     """
     for placeholder, value in rag_info.items():
         prompt = prompt.replace(f"{{{placeholder}}}", value)
-    return system_message
+    return prompt
